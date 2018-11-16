7f1b2e9ed580bd343b1ab6cf914aa6a296d3c1a6
Bernd Lorenzen <bernd.lorenzen@stud.fh-flensburg.de>
2018-11-16 14:55:17 +0100

    parse Templates

--- drückMich.go
+++ drückMich.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-
+	"path/filepath"
 )
 
+//get working Directory
+
+var t = template.Must(template.ParseFiles(filepath.Join("./", "template", "head.html"),
+	filepath.Join("./", "template", "login.html"), filepath.Join("./", "template", "bookmarks.html"),
+	filepath.Join("./", "template", "end.html")))
+
 func main() {
-	http.HandleFunc("/drückMich",pressMeHandler)
-	http.HandleFunc("/Url",urlAjaxHandler)
-	http.ListenAndServe(":4242",nil)
+
+	http.HandleFunc("/drückMich", pressMeHandler)
+	http.HandleFunc("/Url", urlAjaxHandler)
+	http.ListenAndServe(":4242", nil)
 }
 func urlAjaxHandler(writer http.ResponseWriter, request *http.Request) {
-	url:=request.URL.Query().Get("url")
+	url := request.URL.Query().Get("url")
 	fmt.Println(url)
-
-
 
 }
 func pressMeHandler(writer http.ResponseWriter, request *http.Request) {
-	
+
 }
